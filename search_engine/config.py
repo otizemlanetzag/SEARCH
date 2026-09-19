06ca045bf493e48acfccc5ef66947b56d78ec7c7
otizemlanetzag <urielhetzroni@gmail.com>
2026-09-19 09:08:15 +0300

    Use current Common Crawl collection

--- search_engine/config.py
+++ search_engine/config.py
@@ -1,5 +1,5 @@
 import os
 
-COMMON_CRAWL_INDEX = os.getenv("COMMON_CRAWL_INDEX", "CC-MAIN-2026-30")
-WET_LIMIT = int(os.getenv("WET_LIMIT", "100"))
+COMMON_CRAWL_INDEX = os.getenv("COMMON_CRAWL_INDEX", "CC-MAIN-2026-34")
+WET_LIMIT = int(os.getenv("WET_LIMIT", "2"))
 DB_PATH = os.getenv("SEARCH_DB", "search.db")
